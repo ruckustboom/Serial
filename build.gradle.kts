3bf55ad5d741c81752c8344104e385c3b4632bd6
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2021-09-11 12:05:08 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.30"
+    id("maven-publish")
+}
+
+group = "ruckustboom"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    explicitApi()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "16"
+    kotlinOptions.freeCompilerArgs += "-Xjvm-default=all"
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
