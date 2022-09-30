f637e503ec7730e63d7f3a5397a00358307cbe5b
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2022-09-30 14:20:11 -0600

    Merge stream tests

--- src/test/kotlin/TestStreams.kt
+++ src/test/kotlin/TestStreams.kt
@@ -1,5 +1,7 @@
 package serial
 
+import java.io.InputStream
+import java.io.OutputStream
 import kotlin.random.Random
 import kotlin.random.nextUInt
 import kotlin.random.nextULong
@@ -7,7 +9,9 @@ import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
-class TestPrimitives {
+class TestStreams {
+    // Primitives
+
     @Test
     fun testBytes() {
         val values = ByteArray(256, Int::toByte)
@@ -116,6 +120,8 @@ class TestPrimitives {
         assertEquals(values, deserialized)
     }
 
+    private enum class TestEnum { A, B, C, D }
+
     @Test
     fun testEnums() {
         val values = enumValues<TestEnum>().toList()
@@ -140,14 +146,72 @@ class TestPrimitives {
 
         val serializedInts = makeByteArray { for (value in values) writeEnumByOrdinal(value) }
         assertEquals(16, serializedInts.size)
-        val deserializedInts = serializedInts.asInputStream { List(values.size) { readEnumByOrdinal<TestEnum>() } }
+        val deserializedInts =
+            serializedInts.asInputStream { List(values.size) { readEnumByOrdinal<TestEnum>() } }
         assertEquals(values, deserializedInts)
 
         val serializedAuto = makeByteArray { for (value in values) writeEnumByOrdinalAuto(value) }
         assertEquals(4, serializedAuto.size)
-        val deserializedAuto = serializedAuto.asInputStream { List(values.size) { readEnumByOrdinalAuto<TestEnum>() } }
+        val deserializedAuto =
+            serializedAuto.asInputStream { List(values.size) { readEnumByOrdinalAuto<TestEnum>() } }
         assertEquals(values, deserializedAuto)
     }
 
-    private enum class TestEnum { A, B, C, D }
+    // Wrappers
+
+    @Test
+    fun testNullable() {
+        val five = makeByteArray { writeNullable(5, OutputStream::writeInt) }
+        assertEquals(5, five.size)
+        assertEquals(5, five.asInputStream { readNullable(InputStream::readInt) })
+
+        val nil = makeByteArray { writeNullable(null, OutputStream::writeInt) }
+        assertEquals(1, nil.size)
+        assertEquals(null, nil.asInputStream { readNullable(InputStream::readInt) })
+    }
+
+    @Test
+    fun testRepeat() {
+        assertContentEquals(
+            IntArray(20) { it },
+            fullWriteAndRead(
+                { repeatWrite(20, OutputStream::writeInt) },
+                {
+                    val ints = IntArray(20)
+                    repeatRead { ints[it] = readInt() }
+                    ints
+                },
+            ),
+        )
+    }
+
+    @Test
+    fun testLists() {
+        val values = List(20) { it.toString(2) }
+        val serialized = makeByteArray { writeValues(values, OutputStream::writeString) }
+        assertEquals(154, serialized.size)
+        assertEquals(values, serialized.asInputStream { readValues(InputStream::readString) })
+        assertEquals(values, serialized.asInputStream {
+            val x = mutableListOf<String>()
+            readValuesTo(x, InputStream::readString)
+            x
+        })
+        assertEquals(values, serialized.asInputStream {
+            val x = mutableListOf<String>()
+            readValues(InputStream::readString, x::add)
+            x
+        })
+    }
+
+    @Test
+    fun testMaps() {
+        val map = mapOf("one" to 1, "two" to 2, "three" to 3)
+        val serialized = makeByteArray {
+            writeMap(map, OutputStream::writeString, OutputStream::writeInt)
+        }
+        assertEquals(39, serialized.size)
+        assertEquals(map, serialized.asInputStream {
+            readMap({ readString() }, { readInt() })
+        })
+    }
 }
